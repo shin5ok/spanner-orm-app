29766d94d53be50f243fabd47ae71598e39a1c83
Shingo <kawanos@google.com>
2022-04-19 18:04:00 +0900

    fix it

--- 03-Deploy.sh
+++ 03-Deploy.sh
@@ -1,2 +1,2 @@
-CONN=$(gcloud spanner databases describe testdb --instance=test-instance --format=json | jq .name)
+CONN=$(gcloud spanner databases describe testdb --instance=test-instance --format=json | jq .name -r)
 gcloud run deploy --source=. spanner-orm-app --set-env-vars=CONN=$CONN --region=us-central1 --no-allow-unauthenticated
